2c2879101e6ec7fa296aff15f73332aec78a498c
DarshanMistari <mistaridarshan2@gmail.com>
2024-05-10 08:57:07 +0530

    [ADDED]: Stack POP Operation

---
+++ Stack Pop Operation.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+int stack[5];
+int top=-1 , n=5;
+
+void push(int x)
+{
+	if(top==n-1)
+	{
+		cout<<"Stack is Overflow";
+	}
+	else
+	{
+		top++;
+		stack[top]=x;
+	}
+}
+
+void pop()
+{
+	if(top==-1)
+	{
+		cout<<"Stack is Empty";
+	}
+	else
+	{
+		cout<<"Stak is Pop the Elements :"<<stack[top];
+		top--;
+	}
+}
+
+
+void display()
+{
+	if(top <= 0)
+	{
+		cout<<"Stack is Empty";
+	}
+	else
+	{
+		cout<<"Stack is push the Elements :"<<endl;
+		for(int i=top; i>=0; i--)
+		{
+			cout<<stack[i]<<endl;
+		}
+	}
+}
+
+
+int main()
+{
+	push(1);
+	push(2);
+	push(3);
+	push(4);
+	push(5);
+	display();
+	pop();
+}
